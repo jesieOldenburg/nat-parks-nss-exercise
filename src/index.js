e9d13065b3eb836f7d0694fee4f0df91311ddab0
Jesie Oldenburg <oldesdev@outlook.com>
2019-11-13 03:48:56 -0600

    Continuing to build dynamic articles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 console.log('index.js is here');
+const articleContainer = document.querySelector('#article-container')
 
 function getData(url) {
     fetch(url)
-        .then(parks => parks.json())
-        .then(parsedData => {
-            console.log('parsed park data', parsedData)
-        })
+    .then(parks => parks.json())
+    .then(parsedData => {
+        console.table(parsedData)
+        articleContainer.innerHTML = buildArticles(parsedData.name, parsedData.state)
+    })
 }
 
 getData(`http://localhost:8088/parks`)
+
+const buildArticles = (name, state) => {
+    return /*html*/ `
+        <div class="column">
+            <article class="park-article">
+                <h3 class="park-name">${name}</h3>
+                <p class="park-location">${state}</p>
+            </article>
+        </div>
+    `
+}
+
