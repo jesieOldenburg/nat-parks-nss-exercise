fd259ac7f2656e28cad7fd0f141ed902f3a5a818
Jesie Oldenburg <oldesdev@outlook.com>
2019-11-13 22:32:39 -0600

    park data populating to article elements

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 console.log('index.js is here');
-const articleContainer = document.querySelector('#article-container')
+const articleContainer = document.querySelector("#article-container")
+console.log('articleContainer', articleContainer)
+
+function createArticle (park) {
+    return /*html*/ `
+            <div class="column article">
+                <article class="park-article">
+                    <h3 class="park-name">${park.name}</h3>
+                    <p class="park-location">${park.state}</p>
+                </article>
+            </div>
+    `
+}
+
+function appendToDOM (data) {
+    articleContainer.innerHTML += data
+}
 
 function getData(url) {
     fetch(url)
     .then(parks => parks.json())
     .then(parsedData => {
         console.table(parsedData)
-        articleContainer.innerHTML = buildArticles(parsedData.name, parsedData.state)
+        parsedData.forEach( park => {
+            // console.log(park.name, park.state)
+            const populatedHTML = createArticle(park)
+            console.log(populatedHTML)
+            appendToDOM(populatedHTML)
+        });
     })
 }
-
 getData(`http://localhost:8088/parks`)
-
-const buildArticles = (name, state) => {
-    return /*html*/ `
-        <div class="column">
-            <article class="park-article">
-                <h3 class="park-name">${name}</h3>
-                <p class="park-location">${state}</p>
-            </article>
-        </div>
-    `
-}
-
