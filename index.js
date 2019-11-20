e7dbd88a5e3a1858981244dcd017d0f29cc49ac8
Jesie Oldenburg <oldesdev@outlook.com>
2019-11-20 01:36:13 -0600

    added styling to article elements; added logic to assign classes to articles

--- index.js
+++ index.js
@@ -3,18 +3,42 @@ const articleContainer = document.querySelector("#article-container")
 // console.log('articleContainer', articleContainer)
 
 function createArticle (park) {
-    
+// console.log("TCL: createArticle -> park", park)    
+const parkTitle = document.getElementsByClassName('park-name')
+if (park.visited == true) {
     return /*html*/ `
-            <div class="column article">
-                <article class="park-article">
-                    <h3 class="park-name">${park.name}</h3>
-                    <p class="park-location">${park.state}</p>
-                </article>
-            </div>
-    `
+    <div class="column article">
+        <article class="park-article">
+            <h3 class="park-name visited-park">${park.name}</h3>
+            <p class="park-location">${park.state}</p>
+        </article>
+    </div>
+`
+    // parkTitle.classList.add('visited') 
+    // parkTitle.classList.add('visited-park')
+} else {
+    return /*html*/ `
+    <div class="column article">
+        <article class="park-article">
+            <h3 class="park-name not-visited">${park.name}</h3>
+            <p class="park-location">${park.state}</p>
+        </article>
+    </div>
+`
+    // parkTitle.className = 'not-visited'
+}
+    // return /*html*/ `
+    //         <div class="column article">
+    //             <article class="park-article">
+    //                 <h3 class="park-name">${park.name}</h3>
+    //                 <p class="park-location">${park.state}</p>
+    //             </article>
+    //         </div>
+    // `
 }
 
 function appendToDOM (data) {
+    // updateStyles(data)
     articleContainer.innerHTML += data
 }
 
@@ -25,24 +49,31 @@ function getData(url) {
         console.table(parsedData)
         parsedData.forEach( park => {
             // console.log(park.name, park.state)
+            
             const populatedHTML = createArticle(park)
             // console.log(populatedHTML)
             appendToDOM(populatedHTML)
-            updateStyles(park);
+            
+            // updateStyles(park, populatedHTML)
         });
     })
 }
 getData(`http://localhost:8088/parks`)
 
-updateStyles = (park) => {
-    console.log('called update styles')
+updateStyles = (park, html) => {
+    // console.log('called update styles', html)
     const parkTitle = document.getElementsByClassName('park-name')
+    
+    // for (let i = 0, len = parkTitle.length; i < len; i++) {
+    //     if (park.visited == true) {
+    //         console.log(true, html)
+    //         parkTitle.classList.add('visited') 
+    //         // parkTitle.classList.add('visited-park')
+    //     } else {
+    //         console.log(false)
+    //         parkTitle.className = 'not-visited'
+    //     }
+    // }
+    
 
-    if (park.visited == true) {
-        console.log(true)
-        parkTitle.classList.toggle('visited-park')
-    } else {
-        console.log(false)
-        parkTitle.className = 'not-visited'
-    }
 }
